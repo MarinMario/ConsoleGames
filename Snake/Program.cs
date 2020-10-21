053126c8e6b939918718ef66ac0bdcbf3c533e46
Marin Mario <38869813+marinmario@users.noreply.github.com>
2020-10-21 14:21:33 +0300

    Add project files.

---
+++ Snake/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace Snake
+{
+    class Program
+    {
+        static Timer timer;
+
+        [STAThread]
+        public static void Main()
+        {
+            var game = new Game();
+            timer = new Timer(_ => game.Update(), null, 0, 100);
+
+            while (true)
+                game.Input();
+        }
+    }
+}

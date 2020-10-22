7b7e248af33d1a770267d78b23698a1adbbcba0d
Marin Mario <38869813+marinmario@users.noreply.github.com>
2020-10-22 15:09:36 +0300

    feat: pong

---
+++ Pong/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pong
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var game = new Game();
+
+            while (true)
+            {
+                Thread.Sleep(100);
+                game.Update();
+            }
+        }
+    }
+}
